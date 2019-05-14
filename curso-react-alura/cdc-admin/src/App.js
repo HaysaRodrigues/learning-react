1833146553533536cead877c40eb915dfb210a80
Haysa Rodrigues <hjsilva@thoughtworks.com>
2019-05-14 17:02:11 -0300

    update state when register an author and update table

--- curso-react-alura/cdc-admin/src/App.js
+++ curso-react-alura/cdc-admin/src/App.js
@@ -7,10 +7,32 @@ class App extends Component {
 
   constructor(){
     super()
-    this.state = 
-      {lista: []}
+    this.state = {
+        lista: [],
+        nome: '',
+        email:'',
+        senha:''
+    }
+    this.sendForm = this.sendForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+
   }
 
+  
+  setNome(event){
+    this.setState({nome: event.target.value})
+  }
+  
+  setEmail(event){
+    this.setState({email: event.target.value})
+  }
+  
+  setSenha(event){
+    this.setState({senha: event.target.value})
+  }
+  
   componentDidMount(){
     $.ajax({
       url:'http://cdc-react.herokuapp.com/api/autores',
@@ -21,74 +43,100 @@ class App extends Component {
     })
   }
 
+  sendForm(evento){
+    evento.preventDefault();    
+    $.ajax({
+      url:'http://cdc-react.herokuapp.com/api/autores',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+      success: function(resposta){
+        this.setState({lista: resposta})
+       },
+      error: function(resposta){
+        console.log("erro");
+      }      
+    });
+  }
+  
   render() {
     return (
       <div id="layout">
-          <a href="#menu" id="menuLink" className="menu-link">
-            <span></span>
-          </a>
-        <div id="menu">
-            <div className="pure-menu">
-                <a className="pure-menu-heading" href="http://localhost:3000">Company</a>
-                <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Autor</a></li>
-                    <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Livro</a></li>
-                </ul>
-            </div>
+        <a href="#menu" id="menuLink" className="menu-link">
+          <span></span>
+        </a>
+      <div id="menu">
+        <div className="pure-menu">
+            <a className="pure-menu-heading" href="http://localhost:3000">Company</a>
+            <ul className="pure-menu-list">
+                <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Home</a></li>
+                <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Autor</a></li>
+                <li className="pure-menu-item"><a href="http://localhost:3000" className="pure-menu-link">Livro</a></li>
+            </ul>
         </div>
-        <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
-            </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha"  />                                      
-                  </div>
-                  <div className="pure-control-group">                                  
-                    <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
-                </form>             
+      </div>
+      <div id="main">
+        <div className="header">
+          <h1>Cadastro de Autores</h1>
+        </div>
+        <div className="content" id="content">
+          <div className="pure-form pure-form-aligned">
+            <form className="pure-form pure-form-aligned" onSubmit={this.sendForm.bind(this)} >
+              <div className="pure-control-group">
+                <label htmlFor="nome">Nome</label> 
+                <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />                  
+              </div>
+              <div className="pure-control-group">
+                <label htmlFor="email">Email</label> 
+                <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}  />                  
+              </div>
+              <div className="pure-control-group">
+                <label htmlFor="senha">Senha</label> 
+                <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />                                      
+              </div>
+              <div className="pure-control-group">                                  
+                <label></label> 
+                <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+              </div>
+            </form>             
+          </div>  
+          <div>  
 
-              </div>  
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      {
-                        this.state.lista.map(function(a){
-                          return (
-                            <tr key={a.id}>
-                              <td>{a.nome}</td>                
-                              <td>{a.email}</td> 
-                            </tr>
-                          )
-                        })
-                      }               
-                    </tr>
+            <table className="pure-table">
+              <thead>
+                <tr>
+                  <td>Nome</td>
+                  <td>Email</td>
+                </tr>
+              </thead>
+              
+              <tbody>
+                <tr>
+                  <td colSpan={2}>
+                    <table>
+                    <tbody>
+                  {
+                    this.state.lista.map(function(a){
+                      return (
+                        <tr key={a.id}>
+                          <td>{a.nome}</td>                
+                          <td>{a.email}</td> 
+                        </tr>
+                      )
+                    })
+                  } 
                   </tbody>
-                </table> 
-              </div>             
-            </div>
-          </div>            
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table> 
+
+
+          </div>             
+        </div>
+      </div>            
     </div>     
     );
   }
