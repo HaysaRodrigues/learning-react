4eda7522ea3392ce3bdfa9ed483480025804d836
Haysa Rodrigues <hjsilva@thoughtworks.com>
2019-05-22 13:28:39 -0300

    add router in menu options

--- curso-react-alura/cdc-admin/src/index.js
+++ curso-react-alura/cdc-admin/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+(
+  <Router>
+    <Route exact path="/" component={App}/>
+    <Route path="/autor" />
+    <Route path="/livro" />
+  </Router>
+)
+, document.getElementById('root'));
 serviceWorker.unregister();
